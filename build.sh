0ab5a5ce0d6d345714f0d037354851e9160e0b00
Hemant Mohane <hemantmohane29@gmail.com>
2026-06-19 11:11:49 +0530

    Fix: add WHITENOISE_ROOT, fix STATIC_URL slash, clear collectstatic on build

--- build.sh
+++ build.sh
@@ -7,7 +7,6 @@ echo "=== Python: $(python --version) | Pip: $(pip --version) ==="
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
-# Make start.sh executable
 chmod +x start.sh
--- config/settings.py
+++ config/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
@@ -132,6 +132,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+WHITENOISE_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
